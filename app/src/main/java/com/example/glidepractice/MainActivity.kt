0a6d555ff0f741b9436ed6b7568b9e038fbafe83
Sohee <sohee3480@gmail.com>
2021-06-14 16:20:06 +0900

    웹 상의 이미지를 불러오기(Glide 활용)  - 인터넷 권한 획득 체험

--- app/src/main/java/com/example/glidepractice/MainActivity.kt
+++ app/src/main/java/com/example/glidepractice/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.glidepractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        인터넷에 있는 이미지를 이미지뷰에 바로 대입
+
+        Glide.with(this).load("https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png\uFEFF").into(GoogleImg)
     }
 }
